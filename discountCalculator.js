4ad2f00c804b556b2c62ff7993dcbc14a68cef70
nahashon maina <nahmbuthia@gmail.com>
2026-09-26 00:02:31 +0300

    new

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,10 +1,12 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger;
     let totalPrice = 0;
-    debugger
     for (let i = 1; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
-
+    
+    debugger;
+    
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
